d49279df24ee91960a590c5d5ec3dedd3e19ed0a
guillevr777 <gs.villanueva@iesnervion.es>
2025-04-02 13:35:03 +0200

    Update controlador.js

--- scripts/controlador.js
+++ scripts/controlador.js
@@ -4,4 +4,11 @@ class controlador {
         this.vista = new vista();
     }
 
+    iniciar() {
+        this.vista.iniciar();
+        this.vista.btnAgregar.addEventListener("click", () => this.agregarProducto());
+        this.vista.btnEliminar.addEventListener("click", () => this.eliminarProducto());
+        this.vista.btnModificar.addEventListener("click", () => this.modificarProducto());
+        this.vista.btnBuscar.addEventListener("click", () => this.buscarProducto());
+    }
 }
