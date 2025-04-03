e0e03962969eea70957b4566e2fcff7b086cd575
robertomorano <roberto.morano@iesnervion.es>
2025-04-03 13:53:56 +0200

    Update equipo.js

---
+++ scripts/equipo.js
@@ -0,0 +1,21 @@
+class Equipo{
+    constructor(id,nombre,ciudad, estadio) {
+        this.nombre = nombre;
+        this.id = id;
+        this.ciudad = ciudad;  
+        this.estadio = estadio;
+    }
+    getId() {
+        return this.id;
+    }
+    getNombre() {
+        return this.nombre;
+    }
+    getCiudad() {
+        return this.ciudad;
+    }
+    getEstadio() {
+        return this.estadio;
+    }
+    
+}
