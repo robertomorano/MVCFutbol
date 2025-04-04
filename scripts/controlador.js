44ca265b871fd8bf0bb5eba1bd84c6f8590a8ec4
guillevr777 <gs.villanueva@iesnervion.es>
2025-04-04 09:10:30 +0200

    a

--- scripts/controlador.js
+++ scripts/controlador.js
@@ -11,4 +11,36 @@ class controlador {
         this.vista.btnModificar.addEventListener("click", () => this.modificarProducto());
         this.vista.btnBuscar.addEventListener("click", () => this.buscarProducto());
     }
+
+     //agregar un jugador con sus atributos correspondientes a un equipo
+     agregarJugador() {
+        if () {
+            return;
+        }
+        this.modelo.agregarJugador(nombre, posicion, nacimiento);
+    }
+
+    //agregar un equipo con sus atributos correspondientes
+    agregarEquipo() {
+        if () {
+            return;
+        }
+        this.modelo.agregarEquipo(nombre, ciudad, estadio);
+    }
+
+    //asignar un jugador a un equipo
+    asignarJugadorAEquipo() {
+        if () {
+            return;
+        }
+        this.modelo.asignarJugadorAEquipo(idJugador, idEquipo);
+    }
+
+    //Mostrar estadisticas de los jugadores de un equipo
+    mostrarEstadisticas() {
+        if () {
+            return;
+        }
+        this.modelo.mostrarEstadisticas(idEquipo);
+    }
 }
