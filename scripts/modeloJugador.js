12576e9a7657a7cc26c1c7a4bdf24856c2eaae7b
robertomorano <roberto.morano@iesnervion.es>
2025-04-07 13:52:45 +0200

    todo-txt

--- scripts/modeloJugador.js
+++ scripts/modeloJugador.js
@@ -14,33 +14,46 @@ class JugadorModel{
         return this.jugadores;
     }
     
+    // Añadir jugadores a base de datos desde el formulario de añadir jugador
     addPlayer(nombre,posicion,fechaNacimiento, imagen){
+        let url = "";
+        
+        url = URL.createObjectURL(imagen);
         let index = this.jugadores.length;
+        // Iniciar el equipo a null despues con otra funcion se le asignara el id del equipo
         let player = new Jugador(index,nombre,posicion,fechaNacimiento, null ,imagen);
         this.jugadores.push(player);
         this.actualizarPlayerLocalStorage();
+        return true;
     }
+
+    // Añadir jugadores a base de datos desde localStorage hace falta crearlos de nuevo a partir de la clase Jugador
+    // para que se puedan usar los metodos de la clase Jugador 
     addPlayerLocalStorage(nombre, posicion, fechaNacimiento, idEquipo, imagen){
         let player = new Jugador(this.jugadores.length,nombre,posicion,fechaNacimiento, idEquipo ,imagen);
         this.jugadores.push(player);
 
     }
     
-    
+    //Guardar nuevos jugadores en el localStorage
+    // para que se mantengan al recargar la pagina
     actualizarPlayerLocalStorage(){
         
         localStorage.setItem("jugadores", JSON.stringify(this.jugadores));
     }
     
-    
+    // Añadir un equipo a un jugador
+    // para que se pueda filtrar por equipo
     añadirEquipo(id, idEquipo){
         
         this.jugadores.forEach(element => {
             if(element.getId() === id){
                 element.setIdEquipo(idEquipo);
+                return true;
             }
             
         });
+
     }
     
     
