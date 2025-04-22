9826d28734d7d6c863859345506280a30136c077
guillevr777 <gs.villanueva@iesnervion.es>
2025-04-22 23:28:05 +0200

    Update controlador.js

--- scripts/controlador.js
+++ scripts/controlador.js
@@ -162,6 +162,9 @@ class Controlador {
     }
 
     obtenerEquipoPorId(id) {
+        if (id == -1) {
+            return "Asigna Jugador";
+        }
         console.log("Intentando obtener equipo con ID:", id);
         const equipo = this.modeloEquipos.getEquipoPorId(id);
         console.log("Equipo encontrado:", equipo);
